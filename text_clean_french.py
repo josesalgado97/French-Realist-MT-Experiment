a7daa13a8df6ff36b7fd7bf68c93cbb3dca5871a
josesalgado97 <35471530+josesalgado97@users.noreply.github.com>
2020-05-10 20:11:27 +0200

    Add files via upload

---
+++ text_clean_french.py
@@ -0,0 +1,23 @@
+import os
+import glob
+
+path = "/Users/zeca/Desktop/corpus_cleaning/texts_french/*.txt"
+
+for file in glob.glob(path):            # iterate over all files
+    with open(file, "r") as input_file:
+        filename = os.path.basename(file)   # get input file name
+        output = ('%s_clean.txt' % filename)     # meaningfully name the output file
+        with open(output, "w") as output:
+            for line in input_file:
+                line = line.replace('–', '')
+                line = line.replace('«', '')    # replace problematic characters
+                line = line.replace('»', '')
+                line = line.strip()     # clear white space
+                try:
+                    int(line)  # check if line is a number
+                except ValueError:
+                    output.write(line + "\n")
+
+
+
+
